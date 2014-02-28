d62c7b1c4fcbadf715d1ce1a28786f8964ac39b6
Joshua Chin <joshuarchin@gmail.com>
2014-02-28 12:31:31 -0500

    added matrix lib

--- main.js
+++ main.js
@@ -1,15 +1,28 @@
 window.onload = function(){
     var canvas = document.getElementById("canvas");
     var raster = new engine2D.raster(canvas);
-    var p0 = {x:100,y:100};
+    var p0 = [100,100,0,1];
+    var p1 = [100,-100,0,1];
+    var p2 = [-100,-100,0,1];
+    var p3 = [-100,100,0,1];
+    var ps = [p0,p1,p2,p3];
     var angle = 0;
     var red = {r:255, g:0, b:0};
+    var matrix = engine2D.matrix;
     setInterval(function(){
-	var p1 = {x:(50*Math.sin(angle)+p0.x)|0,
-		  y:(50*Math.cos(angle)+p0.y)|0};
 	angle += .1;
 	raster.clear();
-	raster.drawLine(p0,p1,red);
+	var pp = ps.map(
+	    function(v){
+		return matrix.mulVector(
+		    matrix.translate(200,200,0),
+		    matrix.mulVector(
+			matrix.rotationZ(angle),
+			v));});
+	raster.drawLine(pp[0],pp[1],red);
+	raster.drawLine(pp[1],pp[2],red);
+	raster.drawLine(pp[2],pp[3],red);
+	raster.drawLine(pp[3],pp[0],red);
 	raster.render();
     }, 30);
 };
--- renderer.js
+++ renderer.js
@@ -49,50 +49,132 @@ var engine2D=new function(){
 	};
 
 	this.draw = function(point, color){
-	    if(0<=point.x<raster.width && 0<=point.y<raster.height){
-		buffer32[y*width+x] = colorToInt(color);
-	    }
+	    raster.fastDraw(point.x, point.y, colorToInt(color));
 	};
 
 	this.fastDraw = function(x, y, colorInt){
-	    if(0<=x<raster.width && 0<=y<raster.height){
+	    if(0<=x && x<raster.width && 0<=y && y<raster.height){
 		buffer32[y*raster.width+x] = colorInt;
 	    }
-	}
+	};
 
 	this.drawLine = function(p1, p2, color){
-	    var colorInt = colorToInt(color);
-	    var dx = Math.abs(p1.x-p2.x);
-	    var dy = Math.abs(p1.y-p2.y);
+	    raster.fastDrawLine(p1[0],p1[1], p2[0],p2[1], colorToInt(color));
+	};
+
+	this.fastDrawLine = function(x1,y1,x2,y2, colorInt){
+	    x1=x1|0,x2=x2|0,y1=y1|0,y2=y2|0,colorInt=colorInt|0;
+	    var dx = Math.abs(x1-x2);
+	    var dy = Math.abs(y1-y2);
 	    var sx, sy;
-	    if(p2.x > p1.x){sx=1;}else{sx=-1;};
-	    if(p2.y > p1.y){sy=1;}else{sy=-1;};
+	    if(x2 > x1){sx=1;}else{sx=-1;};
+	    if(y2 > y1){sy=1;}else{sy=-1;};
 	    var err = dy - dx;
-	    var x = p1.x, y = p1.y;
 	    while(true){
-		if(x==p2.x && y==p2.y){
-		    raster.fastDraw(x, y, colorInt);
+		if(x1==x2 && y1==y2){
+		    raster.fastDraw(x1, y1, colorInt);
 		    break;
 		}
 		if(2*err<dy){
 		    err += dy;
-		    x += sx;
+		    x1 += sx;
 		}
-		if(x==p2.x && y==p2.y){
-		    raster.fastDraw(x, y, colorInt);
+		if(x1==x2 && y1==y2){
+		    raster.fastDraw(x1, y1, colorInt);
 		    break;
 		}
 		if(2*err>-dx){
 		    err -= dx;
-		    y += sy;
+		    y1 += sy;
 		}
-		raster.fastDraw(x, y, colorInt);
+		raster.fastDraw(x1, y1, colorInt);
 	    }
 	};
 	
     };
 
-    this.matrix = function(){
+    this.matrix = new function(){
+	
+	var matrix = this;
+
+	this.ID = [
+	    1,0,0,0,
+	    0,1,0,0,
+	    0,0,1,0,
+	    0,0,0,1];
+
+	this.rotationX= function(x){
+	    var sin = Math.sin(x), cos = Math.cos(x);
+	    return [
+		1,  0,   0,0,
+		0,cos,-sin,0,
+		0,sin, cos,0,
+		0,  0,   0,1];
+	};
+
+	this.rotationY= function(x){
+	    var sin = Math.sin(x), cos = Math.cos(x);
+	    return [
+		cos,0,-sin,0,
+		0,  1,   0,0,
+		sin,0, cos,0,
+		0,  0,   0,1];
+	};
+
+	this.rotationZ= function(x){
+	    var sin = Math.sin(x), cos = Math.cos(x);
+	    return [
+		cos,-sin,0,0,
+		sin, cos,0,0,
+		0  ,   0,1,0,
+		0  ,   0,0,1];
+	};
+
+	this.scale = function(x,y,z){
+	    return [
+		x,0,0,0,
+		0,y,0,0,
+		0,0,z,0,
+		0,0,0,1];
+	};
+
+	this.translate = function(x,y,z){
+	    return [
+		1,0,0,x,
+		0,1,0,y,
+		0,0,1,z,
+		0,0,0,1];
+	};
+
+	this.mulMatrix = function(M1, M2){
+	    var out = [];
+	    for(var i=0;i<4;i++){
+		for(var j=0;j<4;j++){
+		    var sum = 0;
+		    for(var k=0;k<4;k++){
+			sum += M1[4*i+k] * M2[j+4*k];
+		    }
+		    out[4*i+j] = sum;
+		}
+	    }
+	    return out;
+	};
+
+	this.mulVector = function(M, v){
+	    var out = [];
+	    for(var i=0;i<4;i++){
+		var sum = 0;
+		for(var j=0;j<4;j++){
+		    sum += M[4*i+j] * v[j];
+		}
+		out[i] = sum;
+	    }
+	    return out;
+ 	};
+
+	this.mulScalar = function(s, M){
+	    return M.map(function(x){return s*x;});
+	};
 
     }();
 
@@ -107,7 +189,7 @@ var engine2D=new function(){
 	this.x = x;
 	this.y = y;
 	this.z = z;
-	this.position = new Array(x,y,z,1);
+	this.values = new Array(x,y,z,1);
     };
 
 }();
